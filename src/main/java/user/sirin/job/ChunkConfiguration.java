fda2ab9c7c507fef83da97ab0638c42e425af8f1
sirin0762 <tlsdydwls123@gmail.com>
2023-05-28 14:58:09 +0900

    FEAT : Chunk 개념 및 API 학습

---
+++ src/main/java/user/sirin/job/ChunkConfiguration.java
@@ -0,0 +1,45 @@
+package user.sirin.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.item.support.ListItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ChunkConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkJob() {
+        return jobBuilderFactory.get("chunkJob")
+            .start(chunkStep1())
+            .build();
+
+    }
+
+    @Bean
+    public Step chunkStep1() {
+        return stepBuilderFactory.get("chunkStep1")
+            .<String, String> chunk(5)
+            .reader(new ListItemReader<>(List.of("h")))
+            .processor((ItemProcessor<String, String>) item -> item + "1")
+            .writer(items -> log.info("items = {}", items))
+            .build();
+    }
+
+}
